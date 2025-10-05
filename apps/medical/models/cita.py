06cfbdf78cf795b42167685eb6d0b35e780ed7d0
Jakelin <jakita8889@gmail.com>
2025-10-05 12:03:57 -0600

    Agregar ESTADOS y default a BooleanFields en Cita

--- apps/medical/models/cita.py
+++ apps/medical/models/cita.py
@@ -4,10 +4,25 @@ from apps.medical.models.paciente import Paciente  # ajusta la ruta si es distin
 from apps.medical.models.medico import Medico
 
 class Cita(models.Model):
+    ESTADOS = [
+    ("pendiente", "Pendiente"),
+    ("confirmada", "Confirmada"),
+    ("cancelada", "Cancelada"),
+    ("atendida", "Atendida"),
+]
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     fecha = models.DateField()
     hora = models.TimeField()
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    estado = models.CharField(max_length=20, choices=ESTADOS, default="pendiente")
+
+
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    reminder_sent_24 = models.BooleanField(default=False)   # 👈 evita NULL
+    reminder_sent_48 = models.BooleanField(default=False)
+
 
     def __str__(self):
         return f"{self.paciente} - {self.fecha} {self.hora}"
