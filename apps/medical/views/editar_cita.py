c33cc3da672c2946ec4e0f54941bfb52861c3854
Jakelin <jakita8889@gmail.com>
2025-10-05 13:38:10 -0600

    Fix: editar_cita sin envío de correo para evitar errores 500

--- apps/medical/views/editar_cita.py
+++ apps/medical/views/editar_cita.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from apps.medical.models.cita import Cita
 from apps.forms_cita import CitaForm
-from apps.medical.utils.email_utils import send_cita_notification_email
 
 def editar_cita(request, cita_id):
     # Obtener la cita o devolver 404
@@ -14,12 +13,6 @@ def editar_cita(request, cita_id):
         if form.is_valid():
             form.save()
             messages.success(request, '¡La cita se modificó con éxito!')
-
-            # Enviar notificación por correo
-            if send_cita_notification_email(cita, 'Cita Modificada', 'modified'):
-                messages.info(request, 'Se ha enviado una notificación por correo electrónico.')
-            else:
-                messages.warning(request, 'La cita se modificó, pero no se pudo enviar el correo de notificación.')
 
             # Redirigir al home
             return redirect('home')
